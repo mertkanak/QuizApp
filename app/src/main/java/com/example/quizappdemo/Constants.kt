ba63f7a1f57ff445dffb0183894dbf074894de49
mertkanak <mertkanak77@icloud.com>
2023-10-01 02:23:09 +0300

    Questions added

--- app/src/main/java/com/example/quizappdemo/Constants.kt
+++ app/src/main/java/com/example/quizappdemo/Constants.kt
@@ -6,7 +6,39 @@ object Constants {
 
         val que1 = Question(
             1, "What country does this flag belong to",
-
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Australia", "Armenia","Austria",
+            1,
         )
+        val que2 = Question(
+            1, "What country does this flag belong to",
+            R.drawable.ic_flag_of_australia,
+            "Argentina", "Australia", "Armenia","Brazil",
+            2,
+        )
+        val que3 = Question(
+            1, "What country does this flag belong to",
+            R.drawable.ic_flag_of_belgium,
+            "Belgium", "Australia", "Argentina","Austria",
+            1,
+        )
+        val que4 = Question(
+            1, "What country does this flag belong to",
+            R.drawable.ic_flag_of_brazil,
+            "Argentina", "Australia", "Brazil","Austria",
+            3,
+        )
+        val que5 = Question(
+            1, "What country does this flag belong to",
+            R.drawable.ic_flag_of_fiji,
+            "Argentina", "Fiji", "Armenia","Austria",
+            2,
+        )
+        questionsList.add(que1)
+        questionsList.add(que2)
+        questionsList.add(que3)
+        questionsList.add(que4)
+        questionsList.add(que5)
+        return questionsList
     }
 }
--- app/src/main/java/com/example/quizappdemo/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappdemo/QuizQuestionsActivity.kt
@@ -2,12 +2,14 @@ package com.example.quizappdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-
+        val questionsList = Constants.getQuestions()
+        Log.i("QuestionsList size is", "${questionsList.size}")
     }
 }
