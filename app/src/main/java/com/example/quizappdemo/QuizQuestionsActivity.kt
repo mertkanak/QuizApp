8a4fdeec90d765b57f1343b6b042f2e03feed5b4
mertkanak <mertkanak77@icloud.com>
2023-10-01 17:20:27 +0300

    QuizQuestionsActivity.kt progress

--- app/src/main/java/com/example/quizappdemo/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappdemo/QuizQuestionsActivity.kt
@@ -1,15 +1,36 @@
 package com.example.quizappdemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+
+
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is", "${questionsList.size}")
+
+        for(i in questionsList){
+            Log.e("Questions", i.question)
+        }
     }
 }
