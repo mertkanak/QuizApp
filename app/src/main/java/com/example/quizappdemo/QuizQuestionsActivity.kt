d44b2e54b99990d91b3cba94078024e31b5faff6
mertkanak <mertkanak77@icloud.com>
2023-10-05 09:11:09 +0300

    questionDefault added

--- app/src/main/java/com/example/quizappdemo/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizappdemo/QuizQuestionsActivity.kt
@@ -1,13 +1,17 @@
 package com.example.quizappdemo
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -25,6 +29,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var tvOptionTwo:TextView? = null
     private var tvOptionThree:TextView? = null
     private var tvOptionFour:TextView? = null
+    private var btnSubmit: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
@@ -37,28 +42,56 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
+
+        mQuestionList = Constants.getQuestions()
 
         setQuestion()
+        defaultOptionsView()
 
     }
     private fun setQuestion() {
-        val questionsList = Constants.getQuestions()
-        Log.i("QuestionsList size is", "${questionsList.size}")
 
-        for (i in questionsList) {
-            Log.e("Questions", i.question)
-        }
-
-        var currentPosition = 1
-        val question: Question = questionsList[currentPosition - 1]
+        val question: Question = mQuestionList!![mCurrentPosition - 1]
         ivImage?.setImageResource(question.image)
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
         tvQuestion?.text = question.question
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
+
+        if(mCurrentPosition == mQuestionList!!.size) {
+            btnSubmit?.text = "FINISH"
+        }else{
+            btnSubmit?.text = "SUBMIT"
+        }
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0,it)
+        }
+        tvOptionTwo?.let {
+            options.add(1,it)
+        }
+        tvOptionThree?.let {
+            options.add(2,it)
+        }
+        tvOptionFour?.let {
+            options.add(3,it)
+        }
+
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
     }
 
     override fun onClick(p0: View?) {
